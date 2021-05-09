6753de4637f3c6d3ccc8b03a792ab050e3324696
Lorine ETIENNE <lorine.etienne@student.vinci.be>
2021-05-09 15:31:32 +0200

    feat - modifications main method

--- server/maint.c
+++ server/maint.c
@@ -1,4 +1,19 @@
 #include "utils_v10.h"
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <errno.h>
+
+#define PERM 0666
+//TODO mettre un fichier existant
+#define PATHFILE "test.txt"
+//TODO trouver la taille exacte de la memoire partagée
+#define SHAREDMEMSIZE 42
+
+int createSharedMemory(key_t key){
+    return sshmget(key, SHAREDMEMSIZE, IPC_CREAT | PERM);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -11,16 +26,17 @@ int main(int argc, char **argv)
     int sharedMemID;
     int semaID;
     key_t keylock;
-    int noSemaphore;  //TODO initialiser
+    int noSemaphore = 1;  //TODO bonne valeur ?
     if (type == 1)
     {
-        shs = creercharedmemory(size(int) sizetab, tab[]tab)
-        sharedMemID = sshmget(keylock, sizeof(int), 1);
-        semaID = sem_get(keylock, noSemaphore);
+        keylock = ftok(PATHFILE, 0);
+        sharedMemID = createSharedMemory(keylock);
+        semaID = sem_create(keylock, 1, IPC_CREAT | PERM, 0);
     }
     else if (type == 2)
     {
-        sharedMemID = sshmget(keylock, sizeof(int), 1);
+        keylock = ftok(PATHFILE, 0);
+        sharedMemID = createSharedMemory(keylock);
         sshmdelete(sharedMemID);
         semaID = sem_get(keylock, noSemaphore);
         sem_delete(semaID);
@@ -30,13 +46,15 @@ int main(int argc, char **argv)
         checkCond(argc != 3, "la durée de recurrence n'est pas définie\n");
         int duration = atoi(argv[2]);
         checkNeg(duration, "la duree ne peut pas etre negative\n");
-        sharedMemID = sshmget(keylock, sizeof(int), 1);
+       
+        keylock = ftok(PATHFILE, 0);
+        sharedMemID = createSharedMemory(keylock);
         semaID = sem_get(keylock, noSemaphore);
         sshmat(sharedMemID);
         sem_down0(semaID);
         sleep(duration);
         sem_up0(semaID);
-        //est-ce possible de faire ça --> erreur ????? TODO
+        //
         sshmdt(sshmat(sharedMemID));
     }
     else
