d28495bae606950e87f92474e215f0b08e6b9ec4
baptistehonnay <honnaybaptiste@gmail.com>
2021-05-15 16:04:32 +0200

    rebase

--- server/stat.c
+++ server/stat.c
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <sys/time.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
-
-#include "../utils_v10.h"
-#include "../const.h"
-
-#define NBR_PROGS 1000
-
-#define PERM 0666
-//sharedMem 2 en 1 --> indice du tableau + structure associée
-// aka int + taille de la structure * nbre de structures existantes
-#define SHAREDMEMSIZE sizeof(int) + 1000 * sizeof(struct Programm)
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        printf("Usage : %s <ID du programme>", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    int noProgramme = atoi(argv[1]);
-    if (noProgramme < 0 || noProgramme > NBR_PROGS)
-    {
-        printf("programmeID invalide");
-        exit(EXIT_FAILURE);
-    }
-
-    //SEMA_KEY et SHAREDMEM_KEY definies dans const.h
-    int semaID = sem_get(SEMA_KEY, NO_SEMAPHORE);
-    //va chercher la sharedMem avec une perm de 0 car on ne fait que la rechercher
-    int sharedMemID = sshmget(SHAREDMEM_KEY, SHAREDMEMSIZE, 0);
-    //return la sharedMem
-    void *sharedMemory = sshmat(sharedMemID);
-    //donne la taille de la sharedMem
-    int *tailleLogique = sharedMemory;
-    //pointe vers une structure contenue dans sharedMem
-    struct Programm *tab = sharedMemory + sizeof(int);
-    //lock les ressources
-    sem_down0(semaID);
-    //si pas de programme à cet indice là --> erreur
-    if (noProgramme >= *tailleLogique)
-    {
-        printf("programmeID invalide");
-        exit(EXIT_FAILURE);
-    }
-    printf("%d", tab[noProgramme].programmeID);
-    printf("%s", tab[noProgramme].fichierSource);
-    printf("%s", tab[noProgramme].hasError ? "true" : "false");
-    printf("%d", tab[noProgramme].nombreExcecutions);
-    printf("%d", tab[noProgramme].tempsExcecution);
-
-    // relache la sharedMem
-    sshmdt(sshmat(sharedMemID));
-    // libère la ressource
-    sem_up0(semaID);
-    return EXIT_SUCCESS;
-=======
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -127,5 +62,4 @@ int main(int argc, char **argv)
     // libère la ressource
     sem_up0(semaID);
     return EXIT_SUCCESS;
->>>>>>> f4f1f8a... first client fork version.
 }
