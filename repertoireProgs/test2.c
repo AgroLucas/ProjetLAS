5576c12e2c842717e9d92db49c2cf8beda66d119
lorine.etienne <lorine.etienne@student.vinci.be>
2021-05-15 15:16:49 +0200

    feat - changes in directories

--- repertoireProgs/test2.c
+++
@@ -1,8 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-
-int main () {
-	printf("hello world !  2 ");
-	exit(0);
-}
--- server/server.c
+++ server/server.c
@@ -20,37 +20,29 @@
 #include "../utils_v10.h"
 #include "../const.h"
 
-#define BASE_PROG_PATH "./server/progs/"
-#define MAX_STRING_SIZE_INT 4
+#define BASE_PROG_PATH "./progs/"
 
 
 void requestHandler(void* arg1);
 
-bool createEmptyProgram(Programm* program, char* progName);
-int getFreeIdNumber();
-
-bool getProgram(Programm* program, int programId);
-
-void setProgram(Programm* program);
-
-bool getProgramPath(int programId, char**path, char* extension);
-
-bool executionHandler(Programm* program, int programId, int clientSocket);
-bool prepareExecuteResponse(Programm* program, ExecuteResponse* executeResponse, char** stdout);
-void sendExecuteResponse(ExecuteResponse* executeResponse, char** stdout, int clientSocket);
+void executeProgram(int programId, int clientSocket);
 long getCurrentMs();
 void executeAndGetSdtout(void* arg1, void* arg2);
 
-bool compilationHandler(Programm* program, int clientSocket);
-bool prepareCompilationResponse(Programm* program, CompilationResponse* compilationResponse, char** errors, int clientSocket);
-void sendCompilationResponse(CompilationResponse* compilationResponse, char** errors, int clientSocket);
+Programm getProgram(int programId);
+
+Programm createEmptyProgram(char* progName);
+int getFreeIdNumber();
+
+void modifyProgram(Programm program, int clientSocket);
 void compileAndGetErrors(void* arg1, void* arg2, void* arg3);
 
+char* getProgPath(int programId, char* extension);
 
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		perror("Usage : ./server/server [port]\n");
+		printf("Usage : %s [port]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	
@@ -72,118 +64,57 @@ void requestHandler(void* arg1) {
 	Request request;
 	sread(*clientSocket, &request, sizeof(request));
 
-	Programm* program;
-	bool isProgramAllocated = (request.firstInt == ADD_VALUE) ? createEmptyProgram(program, request.progName) : getProgram(program, request.firstInt);
-	if (isProgramAllocated)
-		(request.firstInt == EXECUTION_VALUE) ? executionHandler(program, request.secondInt, *clientSocket) : compilationHandler(program, *clientSocket);
+	if (request.firstInt == -2)	return executeProgram(request.secondInt, *clientSocket);
+	
+	Programm program = request.firstInt == -1 ? createEmptyProgram(request.progName) : getProgram(request.firstInt);
 
-	//TODO update program on shared memory if needed only
-
-	free(program);
-	sclose(*clientSocket);
+	modifyProgram(program, *clientSocket);
 }
 
 
+void executeProgram(int programId, int clientSocket) {
+	int programState = 1;
+	int executionTime = 0;
+	int exitCode = 0;
 
+	char* executablePath = getProgPath(programId, "out");
 
-bool createEmptyProgram(Programm* program, char* progName) {
-	if ((program = (Programm*)malloc(sizeof(Programm))) == NULL) {
-		perror("Allocation dynamique de program impossible");
-		return false;
-	}
-	program->programmeID = getFreeIdNumber();
-	program->fichierSource = progName;
-	program->hasError = false;
-	program->nombreExcecutions = 0;
-	program->tempsExcecution = 0;
-	return true;
-}
-
-
-//TODO return number of progs in shared memory
-int getFreeIdNumber() {
-	return 0;
-}
-
-//TODO get from shared memory or NULL if does not exist
-bool getProgram(Programm* program, int programId) {
-	if ((program = (Programm*)malloc(sizeof(Programm))) == NULL) {
-		perror("Allocation dynamique de program impossible");
-		return false;
-	}
-	program->programmeID = programId;
-	program->fichierSource = "helloWorld.c";
-	program->hasError = false;
-	program->nombreExcecutions = 0;
-	program->tempsExcecution = 0;
-	return true;
-}
-
-//TODO save program into shared memory
-void setProgram(Programm* program) {
-
-}
-
-
-bool getProgramPath(int programId, char** path, char* extension) {
-	int size = strlen(BASE_PROG_PATH) + MAX_STRING_SIZE_INT + strlen(extension);
-	if ((*path = (char*)malloc(size*sizeof(char))) == NULL) {
-		perror("Allocation dynamique de path impossible");
-		return false;
-	}
-	sprintf(*path, "%s%d.%s", BASE_PROG_PATH, programId, extension);
-	return true;
-}
-
-
-//--- EXECUTION ---
-
-bool executionHandler(Programm* program, int programId, int clientSocket) {
-	ExecuteResponse executeResponse = {programId, GOOD_EXECUTION, 0, 0};
 	char* stdout;
 
-	if (!prepareExecuteResponse(program, &executeResponse, &stdout)) return false;
-
-	if (programm->programState == GOOD_EXECUTION || programm->programState == WRONG_EXECUTION)
-		setProgram(program);
-
-	sendExecuteResponse(&executeResponse, &stdout, clientSocket);
-	
-	free(stdout);
-	return true;
-}
-
-
-bool prepareExecuteResponse(Programm* program, ExecuteResponse* executeResponse, char** stdout) {
-	if (program == NULL)
-		executeResponse->programState = NOT_EXIST;
-	else if (program->hasError)
-		executeResponse->programState = NOT_COMPILE;
+	Programm program = getProgram(programId);
+	if (program.fichierSource == NULL) //program does not exist
+		programState = -2;
+	else if (program.hasError)
+		programState = -1;			//program does not compile
 	else {
-		char* executablePath;
-		if (!getProgramPath(executeResponse->n, &executablePath, "out")) return false;
-
+		//execute prog and get stdout in variable
 		int pipefd[2];
 		spipe(pipefd);
-		int status; 
 
-		long beginTime = getCurrentMs();
+		long ms1 = getCurrentMs();
 		int childId = fork_and_run2(executeAndGetSdtout, executablePath, pipefd);
-		sclose(pipefd[1]);
+		int status;
 		swaitpid(childId, &status, 0);
-		executeResponse->executionTime = getCurrentMs() - beginTime;
+		executionTime = getCurrentMs() - ms1;
 
-		getStringFromInput(stdout, pipefd[0]);
+		sclose(pipefd[1]);
+		getStringFromInput(&stdout, pipefd[0]);
 		sclose(pipefd[0]);
-		executeResponse->exitCode = WEXITSTATUS(status);
-		free(executablePath);
-		
-		if (executeResponse->exitCode != 0)
-			executeResponse->programState = WRONG_EXECUTION;
+		exitCode = WEXITSTATUS(status);
 
-		program->nombreExcecutions++;
-	}
-	return true;
+		if (exitCode != 0)
+			programState =  0;			//program does not end normally
+	}	
+	
+	//TODO extract in method for avoiding nested if
+	//send Response to client
+	ExecuteResponse executeResponse = {programId, programState, executionTime, exitCode};
+	swrite(clientSocket, &executeResponse, sizeof(executeResponse));
+	swrite(clientSocket, stdout, strlen(stdout)*sizeof(char));
+	sshutdown(clientSocket, SHUT_WR);
+	
+	free(stdout);
+	sclose(clientSocket);
 }
 
 
@@ -194,69 +125,79 @@ long getCurrentMs() {
 }
 
 
-
+//todo remove close to have only sexecl - make dup before and after fork and run
 void executeAndGetSdtout(void* arg1, void* arg2) {
 	char* executablePath = arg1;
 	int *pipefd = arg2;
 
 	sclose(pipefd[0]);
+
 	dup2(pipefd[1], 1);
+
 	sexecl(executablePath, NULL);
+
 	sclose(pipefd[1]);
 }
 
 
-void sendExecuteResponse(ExecuteResponse* executeResponse, char** stdout, int clientSocket) {
-	swrite(clientSocket, executeResponse, sizeof(&executeResponse));
-	swrite(clientSocket, *stdout, strlen(*stdout)*sizeof(char));
-	sshutdown(clientSocket, SHUT_WR);
+Programm createEmptyProgram(char* progName) {
+	Programm program;
+	program.programmeID = getFreeIdNumber();
+	program.fichierSource = progName;
+	program.hasError = false;
+	program.nombreExcecutions = 0;
+	program.tempsExcecution = 0;
+
+	//TODO put in shared memory
+
+
+	return program;
+}
+
+//TODO
+int getFreeIdNumber() {
+	//return number of progs in shared memory
+	return 0;
+}
+
+//TODO get from shared memory or NULL if does not exist
+Programm getProgram(int programId) {
+	Programm program = {programId, "helloWorld.c", false, 0, 0};
+	return program;
 }
 
 
-//--- COMPILATION ---
-
-bool compilationHandler(Programm* program, int clientSocket) {
-	CompilationResponse compilationResponse = {program->programmeID, 0};
-	char* errors;
-
-	if (!prepareCompilationResponse(program, &compilationResponse, &errors, clientSocket)) return false;
-	
-	setProgram(program);
-
-	sendCompilationResponse(&compilationResponse, &errors, clientSocket);
-
-	free(errors);
-	return true;
-}
-
-
-bool prepareCompilationResponse(Programm* program, CompilationResponse* compilationResponse, char** errors, int clientSocket) {
-	char* inputPath;
-	if (getProgramPath(program->programmeID, &inputPath, "c")) return false;
-	char* outputPath;
-	if (getProgramPath(program->programmeID, &outputPath, "out")) return false;
+void modifyProgram(Programm program, int clientSocket) {
+	char* inputPath = getProgPath(program.programmeID, "c");
+	char* outputPath = getProgPath(program.programmeID, "out");
 	//write into program file source
-	int fd = sopen(inputPath, O_WRONLY | O_CREAT | O_TRUNC, 0744);
- 	readThenWrite(clientSocket, fd);
-	close(fd);
+	overwriteFromInputIntoClosedOutput(clientSocket, inputPath);
 	//compile and get errors in variable
 	int pipefd[2];
 	spipe(pipefd);
 	int childId = fork_and_run3(compileAndGetErrors, inputPath, outputPath, pipefd);
 	sclose(pipefd[1]);
-	getStringFromInput(errors, pipefd[0]);
+	char* errors;
+	getStringFromInput(&errors, pipefd[0]);
 	sclose(pipefd[0]);
 
 	int status;
 	swaitpid(childId, &status, 0);
 	int exitCode = WEXITSTATUS(status);
 
-	free(inputPath);
-	free(outputPath);
+	if (exitCode != 0) {
+		//TODO update hasError from shared memory
+	}
 
-	program->hasError = (exitCode == 0) ? false : true;
+	//send Response to client
+	CompilationResponse compilationResponse = {program.programmeID, exitCode};
+	swrite(clientSocket, &compilationResponse, sizeof(compilationResponse));
+	swrite(clientSocket, errors, strlen(errors) * sizeof(char));
+	sshutdown(clientSocket, SHUT_WR);
 
-	return true;
+
+	free(errors);
+	sclose(clientSocket);
 }
 
 
@@ -275,8 +216,11 @@ void compileAndGetErrors(void* arg1, void* arg2, void* arg3) {
 }
 
 
-void sendCompilationResponse(CompilationResponse* compilationResponse, char** errors, int clientSocket) {
-	swrite(clientSocket, compilationResponse, sizeof(compilationResponse));
-	swrite(clientSocket, *errors, strlen(*errors) * sizeof(char));
-	sshutdown(clientSocket, SHUT_WR);
+char* getProgPath(int programId, char* extension) {
+	char* stringId;
+	sprintf(stringId,"%d", programId);
+	char* path = strcat(BASE_PROG_PATH, stringId);
+	path = strcat(path, ".");
+	path = strcat(path, extension);
+	return path;
 }
